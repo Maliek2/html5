d9b0b8a8cd32c9c0cfcf13870430fb74adfdbda7
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-11-01 08:26:27 +0200

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,8 +1,44 @@
 
+      window.googletag = window.googletag || {cmd: []};
 
+      // GPT ad slots
+      var interstitialSlot, staticSlot;
 
+      googletag.cmd.push(function() {
+        // Define a web interstitial ad slot.
+        interstitialSlot = googletag.defineOutOfPageSlot(
+            '/6355419/Travel/Europe/France/Paris',
+            googletag.enums.OutOfPageFormat.INTERSTITIAL);
 
-googletag.pubads().setPrivacySettings({
+        // Slot returns null if the page or device does not support interstitials.
+        if (interstitialSlot) {
+          interstitialSlot.addService(googletag.pubads());
+
+          document.getElementById('status').innerText = 'Interstitial is loading...';
+
+          // Add event listener to enable navigation once the interstitial loads.
+          // If this event doesn't fire, try clearing local storage and refreshing
+          // the page.
+          googletag.pubads().addEventListener('slotOnload', function(event) {
+            if (interstitialSlot === event.slot) {
+              document.getElementById('link').style.display = 'block';
+              document.getElementById('status').innerText = 'Interstitial is loaded.';
+            }
+          });
+        }
+
+        // Define static ad slots.
+        staticSlot = googletag.defineSlot(
+            '/6355419/Travel/Europe', [100, 100], 'static-ad-1')
+          .addService(googletag.pubads());
+
+        // Enable SRA and services.
+        googletag.pubads().enableSingleRequest();
+        googletag.enableServices();
+      });
+    
+
+        googletag.pubads().setPrivacySettings({
   restrictDataProcessing: true,
 });
 
